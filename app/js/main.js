8cc2d905b7fbabe5c671fff88ed1281d5827df59
khasenovweb <hasenovkirill93@gmail.com>
2022-08-15 11:33:54 +0500

    ldkjfdlkfj

--- app/js/main.js
+++ app/js/main.js
@@ -15,5 +15,64 @@
 // ==============================//
  
 $(document).ready(function(){
-    
+    var header = new Headhesive(".header", {
+	    offset: 1000,
+	});
+
+    $("[data-scroll]").mPageScroll2id({
+        scrollSpeed: 900,
+        keepHighlightUntilNext: true,
+        offset: 100,
+        onStart:function(){
+            $('[data-mobilnav]').removeClass('active');
+            $('body').removeClass('body_hidden');
+        },
+    });
+
+    $('body').on('click','[data-hamburger]', function(){
+        $('[data-mobilnav]').toggleClass('active');
+        $('body').toggleClass('body_hidden');
+    });
+
+    // Получаем ширину скроллбара
+    function get_scrollbar_width(){
+        let div = document.createElement('div');
+
+        div.style.overflowY = 'scroll';
+        div.style.width = '50px';
+        div.style.height = '50px';
+        
+        // мы должны вставить элемент в документ, иначе размеры будут равны 0
+        document.body.append(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        
+        div.remove();
+        console.log(scrollWidth);
+        
+        return scrollWidth;
+    }
+    // END Получаем ширину скроллбара
+
+    $('[data-modal-show]').click(function(){
+        var id = $(this).attr('data-modal-show');
+        $('[data-modal="'+id+'"]').addClass('active');
+        var scrollbar_width = get_scrollbar_width();
+        $('body').css('padding-right', scrollbar_width+'px');
+        $('body').addClass('body_hidden');
+    });
+
+    $('[data-modal-close]').click(function(){
+        var id = $(this).attr('data-modal-close');
+        $('[data-modal="'+id+'"]').removeClass('active');
+        $('body').css('padding-right', '0');
+        $('body').removeClass('body_hidden');
+    });
+
+    $('[data-modal]').click(function(){
+        var thisvar = $(this);
+        if ($(event.target).closest(".modal").length) return;
+        thisvar.removeClass('active');
+        $('body').css('padding-right', '0');
+        $('body').removeClass('body_hidden')
+    });
 });
